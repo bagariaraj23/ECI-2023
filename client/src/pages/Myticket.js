9ed03d1e3d3e06ec5c8887766f69ad5128e22bef
Bhoogarv18 <bhugarv@gmail.com>
2023-04-16 03:19:25 +0530

    changed the game baby

--- client/src/pages/Myticket.js
+++ client/src/pages/Myticket.js
@@ -1,9 +1,340 @@
-import React from 'react'
+import React from "react";
+import "./xx.css";
 
 const Myticket = () => {
   return (
-    <div>Myticket</div>
-  )
-}
+    <div>
+      <section class="dark">
+        <div class="container py-4">
+          <h1 class="h1 text-center" id="pageHeaderTitle">
+           Help Desk
+          </h1>
 
-export default Myticket
+          <article class="postcard dark blue">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/1000/1000"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text">
+              <h1 class="postcard__title blue">
+                <a href="#">Voter Registration (Online)</a>
+              </h1>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+..
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play blue">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard dark red">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/501/500"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text">
+              <h1 class="postcard__title red">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play red">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard dark green">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/500/501"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text">
+              <h1 class="postcard__title green">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play green">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard dark yellow">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/501/501"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text">
+              <h1 class="postcard__title yellow">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play yellow">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard dark green">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/500/501"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text">
+              <h1 class="postcard__title green">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play green">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+        </div>
+      </section>
+
+      <section class="light">
+        <div class="container py-2">
+          <div class="h1 text-center text-dark" id="pageHeaderTitle">
+          </div>
+
+          <article class="postcard light red">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/501/500"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text t-dark">
+              <h1 class="postcard__title red">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play red">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard light blue">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/1000/1000"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text t-dark">
+              <h1 class="postcard__title blue">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play blue">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard light red">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/501/500"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text t-dark">
+              <h1 class="postcard__title red">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play red">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard light green">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/500/501"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text t-dark">
+              <h1 class="postcard__title green">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play green">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article class="postcard light yellow">
+            <a class="postcard__img_link" href="#">
+              <img
+                class="postcard__img"
+                src="https://picsum.photos/501/501"
+                alt="Image Title"
+              />
+            </a>
+            <div class="postcard__text t-dark">
+              <h1 class="postcard__title yellow">
+                <a href="#">Podcast Title</a>
+              </h1>
+              <div class="postcard__subtitle small"></div>
+              <div class="postcard__bar"></div>
+              <div class="postcard__preview-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, fugiat asperiores inventore beatae accusamus odit
+                minima enim, commodi quia, doloribus eius! Ducimus nemo
+                accusantium maiores velit corrupti tempora reiciendis molestiae
+                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
+                neque nulla unde ipsum dolores nobis enim quidem excepturi,
+                illum quos!
+              </div>
+              <ul class="postcard__tagbox">
+                <li class="tag__item play yellow">
+                  <a href="#">
+                    <i class="fas fa-play mr-2"></i>View More...
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </article>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Myticket;
