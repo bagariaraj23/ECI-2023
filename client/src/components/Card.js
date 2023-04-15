35265b7598f1741216a9453f428d147b18d138cc
Kunal Kabra <kunalkabra04@gmail.com>
2023-04-16 04:42:41 +0530

    Update Card.js

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,34 +6,40 @@ const Card = () => {
     <div>
       <div class="main">
         <ul class="cards">
-          <li class="cards_item">
+        <li class="cards_item">
             <div class="card">
               <div class="card_image">
                 <img
-                  src="https://assets.codepen.io/652/photo-1468777675496-5782faaea55b.jpeg"
+                  src="https://paytmblogcdn.paytm.com/wp-content/uploads/2022/08/Blog-Voter-ID-Documents_Cover-banner_Final-800x500.png"
                   alt="mixed vegetable salad in a mason jar."
                 />
-                <span class="card_price">
-                  <span>$</span>9
-                </span>
               </div>
               <div class="card_content">
-                <h2 class="card_title">Farmstand Salad</h2>
+                <h2 class="card_title">Voter registration</h2>
                 <div class="card_text">
                   <p>
-                    Dig into the freshest veggies of the season! This
-                    salad-in-a-jar features a mixture of leafy greens and
-                    seasonal vegetables, fresh from the farmer's market.
+                  Learn how to register to vote and get a voter registration card. Find your state's registration deadline and how to confirm or change your registration.
                   </p>
                   <hr />
                   <p>
-                    Served with your choice of dressing on the side:{" "}
-                    <strong>housemade ranch</strong>,{" "}
-                    <strong>cherry balsamic vinaigrette</strong>,{" "}
-                    <strong>creamy chipotle</strong>,{" "}
-                    <strong>avocado green goddess</strong>, or{" "}
-                    <strong>honey mustard</strong>. Add your choice of protein
-                    for $2 more.
+                  In India, the minimum age for voter registration is 18 years. Citizens who are 18 years of age or older on the qualifying date are eligible to apply for voter registration.
+                  In India, voter registration can be done through both online and offline modes.
+                  <br></br>
+
+<strong>Online registration:</strong>
+
+Visit the website of the National Voter's Service Portal (NVSP) at https://www.nvsp.in/
+Click on the "Apply online for registration of new voter" option.
+Fill in the required details and upload the necessary documents.
+Submit the application form and note down the reference number for future use.
+<br></br>
+<strong>Offline registration:</strong>
+
+Visit the nearest Electoral Registration Office (ERO) or the Voter Facilitation Center (VFC).
+Collect the Form 6 for new voter registration.
+Fill in the required details and attach the necessary documents.
+Submit the application form to the ERO or VFC officials.
+After submitting the application, the concerned officials will verify the details and documents provided, and upon successful verification, the applicant's name will be added to the electoral roll, and a voter ID card will be issued.
                   </p>
                 </div>
               </div>
@@ -43,30 +49,20 @@ const Card = () => {
             <div class="card">
               <div class="card_image">
                 <img
-                  src="https://assets.codepen.io/652/photo-1468777675496-5782faaea55b.jpeg"
+                  src="https://pbs.twimg.com/media/EKcLuaLXUAAJ7tn.jpg"
                   alt="mixed vegetable salad in a mason jar."
                 />
-                <span class="card_price">
-                  <span>$</span>9
-                </span>
               </div>
               <div class="card_content">
-                <h2 class="card_title">Farmstand Salad</h2>
+                <h2 class="card_title"> Voting and election laws</h2>
                 <div class="card_text">
                   <p>
-                    Dig into the freshest veggies of the season! This
-                    salad-in-a-jar features a mixture of leafy greens and
-                    seasonal vegetables, fresh from the farmer's market.
+                  Learn how campaign contribution limits, accessibility rules, and other federal election laws help protect your voting rights and the election process.
+            Know the voter ID laws in your state before going to the polls.
                   </p>
                   <hr />
                   <p>
-                    Served with your choice of dressing on the side:{" "}
-                    <strong>housemade ranch</strong>,{" "}
-                    <strong>cherry balsamic vinaigrette</strong>,{" "}
-                    <strong>creamy chipotle</strong>,{" "}
-                    <strong>avocado green goddess</strong>, or{" "}
-                    <strong>honey mustard</strong>. Add your choice of protein
-                    for $2 more.
+                  The Constitution of India provides for free and fair elections, which are conducted by an independent Election Commission of India. The Indian electoral system is based on the principle of universal adult suffrage, which means that every citizen of India who is above the age of 18 years has the right to vote. The country has a multi-party system, and political parties are an integral part of the Indian electoral process. The Election Commission of India is responsible for enforcing the election laws and ensuring that the electoral process is conducted in a fair and transparent manner.
                   </p>
                 </div>
               </div>
@@ -76,30 +72,19 @@ const Card = () => {
             <div class="card">
               <div class="card_image">
                 <img
-                  src="https://assets.codepen.io/652/photo-1468777675496-5782faaea55b.jpeg"
+                  src="https://us.123rf.com/450wm/ajijchan/ajijchan2103/ajijchan210300046/170037315-hand-casting-vote-in-electronic-voting-machine.jpg?ver=6"
                   alt="mixed vegetable salad in a mason jar."
                 />
-                <span class="card_price">
-                  <span>$</span>9
-                </span>
               </div>
               <div class="card_content">
-                <h2 class="card_title">Farmstand Salad</h2>
+                <h2 class="card_title"> Decide who to vote for</h2>
                 <div class="card_text">
                   <p>
-                    Dig into the freshest veggies of the season! This
-                    salad-in-a-jar features a mixture of leafy greens and
-                    seasonal vegetables, fresh from the farmer's market.
+                  Learn how to make informed choices by using voter guides and sample ballots to research candidates.Reviewing voter guides and sample ballots before you go to the polls can help you decide who to vote for.
                   </p>
                   <hr />
                   <p>
-                    Served with your choice of dressing on the side:{" "}
-                    <strong>housemade ranch</strong>,{" "}
-                    <strong>cherry balsamic vinaigrette</strong>,{" "}
-                    <strong>creamy chipotle</strong>,{" "}
-                    <strong>avocado green goddess</strong>, or{" "}
-                    <strong>honey mustard</strong>. Add your choice of protein
-                    for $2 more.
+                  Voter guides provide background information on the candidates and ballot measures. They list who you can vote for and offer details on each candidate's experience and goals. They also explain ballot measures, which are specific questions or issues that you can approve or reject.
                   </p>
                 </div>
               </div>
@@ -109,30 +94,20 @@ const Card = () => {
             <div class="card">
               <div class="card_image">
                 <img
-                  src="https://assets.codepen.io/652/photo-1468777675496-5782faaea55b.jpeg"
+                  src="https://images.news18.com/ibnlive/uploads/2022/07/whatsapp-image-2022-07-14-at-4.54.04-pm.jpeg?impolicy=website&width=600&height=600"
                   alt="mixed vegetable salad in a mason jar."
                 />
-                <span class="card_price">
-                  <span>$</span>9
-                </span>
               </div>
               <div class="card_content">
-                <h2 class="card_title">Farmstand Salad</h2>
+                <h2 class="card_title">Presidential election process</h2>
                 <div class="card_text">
                   <p>
-                    Dig into the freshest veggies of the season! This
-                    salad-in-a-jar features a mixture of leafy greens and
-                    seasonal vegetables, fresh from the farmer's market.
+                  Learn about the presidential election process, including the Electoral College, caucuses and primaries, and the national conventions.
+            An election for president of the India happens every five years.
                   </p>
                   <hr />
                   <p>
-                    Served with your choice of dressing on the side:{" "}
-                    <strong>housemade ranch</strong>,{" "}
-                    <strong>cherry balsamic vinaigrette</strong>,{" "}
-                    <strong>creamy chipotle</strong>,{" "}
-                    <strong>avocado green goddess</strong>, or{" "}
-                    <strong>honey mustard</strong>. Add your choice of protein
-                    for $2 more.
+                  The Presidential election process in India is a complex and elaborate procedure that involves the participation of members of the electoral college comprising of elected members of both Houses of Parliament and State Legislative Assemblies. The President is elected indirectly for a term of five years, and the process is conducted by the Election Commission of India. The election process is governed by the provisions of the Constitution of India and the Presidential and Vice-Presidential Elections Act, 1952.
                   </p>
                 </div>
               </div>
@@ -142,30 +117,21 @@ const Card = () => {
             <div class="card">
               <div class="card_image">
                 <img
-                  src="https://assets.codepen.io/652/photo-1468777675496-5782faaea55b.jpeg"
+                  src="https://eduauraapublic.s3.ap-south-1.amazonaws.com/webassets/images/blogs/how-is-elections-held-in-india.jpg"
                   alt="mixed vegetable salad in a mason jar."
                 />
-                <span class="card_price">
-                  <span>$</span>9
-                </span>
               </div>
               <div class="card_content">
-                <h2 class="card_title">Farmstand Salad</h2>
+                <h2 class="card_title">General election process</h2>
                 <div class="card_text">
                   <p>
-                    Dig into the freshest veggies of the season! This
-                    salad-in-a-jar features a mixture of leafy greens and
-                    seasonal vegetables, fresh from the farmer's market.
+                  The general election in India is held every five years to elect members of the Lok Sabha, which is the lower house of the Parliament of India.
+                  The general election process in India is typically carried out in several phases over the course of several weeks. During this time, political parties and their candidates campaign heavily to win the support of voters. This can include rallies, speeches, door-to-door canvassing, and advertising.
+
                   </p>
                   <hr />
                   <p>
-                    Served with your choice of dressing on the side:{" "}
-                    <strong>housemade ranch</strong>,{" "}
-                    <strong>cherry balsamic vinaigrette</strong>,{" "}
-                    <strong>creamy chipotle</strong>,{" "}
-                    <strong>avocado green goddess</strong>, or{" "}
-                    <strong>honey mustard</strong>. Add your choice of protein
-                    for $2 more.
+                    On election day, citizens who are registered to vote in a particular constituency can go to their local polling station to cast their ballot. Once the votes are tallied, the candidate who receives the most votes in each constituency is declared the winner, and they will go on to represent their constituency in the Lok Sabha.
                   </p>
                 </div>
               </div>
@@ -175,30 +141,19 @@ const Card = () => {
             <div class="card">
               <div class="card_image">
                 <img
-                  src="https://assets.codepen.io/652/photo-1468777675496-5782faaea55b.jpeg"
+                  src="https://www.newsclick.in/sites/default/files/styles/amp_1200x675_16_9/public/2022-01/election%20india.PNG?itok=S4skpTUw"
                   alt="mixed vegetable salad in a mason jar."
                 />
-                <span class="card_price">
-                  <span>$</span>9
-                </span>
               </div>
               <div class="card_content">
-                <h2 class="card_title">Farmstand Salad</h2>
+                <h2 class="card_title">Assembly election process</h2>
                 <div class="card_text">
                   <p>
-                    Dig into the freshest veggies of the season! This
-                    salad-in-a-jar features a mixture of leafy greens and
-                    seasonal vegetables, fresh from the farmer's market.
+                  Assembly elections in India are the elections held to elect the members of the legislative assembly of each state and union territory in the country. These elections are conducted after a fixed term of five years, or in case of a dissolution of the assembly by the governor of the state. The members of the legislative assembly are elected by the citizens of the state through a direct voting system, based on the principle of universal adult suffrage.
                   </p>
                   <hr />
                   <p>
-                    Served with your choice of dressing on the side:{" "}
-                    <strong>housemade ranch</strong>,{" "}
-                    <strong>cherry balsamic vinaigrette</strong>,{" "}
-                    <strong>creamy chipotle</strong>,{" "}
-                    <strong>avocado green goddess</strong>, or{" "}
-                    <strong>honey mustard</strong>. Add your choice of protein
-                    for $2 more.
+                  Assembly elections are a crucial event in the Indian political calendar, as they determine the composition of the state government and impact national politics. The Election Commission of India is responsible for conducting these elections, and the process involves the registration of political parties, the nomination of candidates, campaigning, polling, and counting of votes.
                   </p>
                 </div>
               </div>
